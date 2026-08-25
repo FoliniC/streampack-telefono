c1d1b35f7f7c4e4efe677d544336061b12ec3bd3
carlo <carlo@localhost>
2026-08-25 19:19:32 +0200

    Default RTMP endpoint -> MediaMTX :1935/honor for HONOR phone

--- app/src/main/java/io/github/thibaultbee/streampack/app/data/storage/StorageRepository.kt
+++ app/src/main/java/io/github/thibaultbee/streampack/app/data/storage/StorageRepository.kt
@@ -5,6 +5,6 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class StorageRepository {
-    private val _urlStringFlow = MutableStateFlow("rtmp://192.168.0.3:1935/telefono")
+    private val _urlStringFlow = MutableStateFlow("rtmp://192.168.0.3:1935/honor")
     val urlStringFlow: StateFlow<String> = _urlStringFlow.asStateFlow()
 }
